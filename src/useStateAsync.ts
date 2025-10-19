2371c8aba2a8d8c379361b08f4f95fcef02cc637
Ron Newcomb <pscion@yahoo.com>
2025-10-19 13:43:13 -0700

    works with STrict mode

--- src/useStateAsync.ts
+++ src/useStateAsync.ts
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
 
 type RefreshFn<T> = () => Promise<ReturnTupleType<T>>;
 type ReturnTupleType<T> = [T | undefined, boolean, unknown, RefreshFn<T>, Promise<ReturnTupleType<T>>];
@@ -10,27 +10,40 @@ const REFRESH = 3;
 const PROMISE = 4;
 
 /** useAsync without useEffect */
-export function useStateAsync<T>(fnAsync: (() => Promise<T>) | undefined | null | false | 0 | "", depArray: unknown[]): ReturnTupleType<T> {
-  console.log("useStateAsync", depArray);
+export function useStateAsync<T>(fnAsync: (() => Promise<T>) | undefined | null | false | 0 | "", dependencies: unknown[]): ReturnTupleType<T> {
+  console.log("Re-renderin useStateAsync", dependencies);
   const [tuple, setTuple] = useState<ReturnTupleType<T>>(constructEmpty);
+  const args = useRef<unknown[] | undefined>(undefined); // in react STRICT MODE, remembering and checking this to see if we need to issue I/O avoids extra-fetch
 
+  // manaul refresh, always happens
   tuple[REFRESH] = useCallback<RefreshFn<T>>(() => {
-    console.log("CALLING");
-    if (fnAsync) {
-      tuple[PROMISE] = fnAsync()
-        .then<ReturnTupleType<T>>(data => [data, false, undefined, tuple[REFRESH], tuple[PROMISE]])
-        .catch<ReturnTupleType<T>>((err: unknown) => [undefined, false, err, tuple[REFRESH], tuple[PROMISE]])
-        .then(tuple => {
-          setTuple(tuple);
-          console.log("RETURNING");
-          return tuple;
-        });
-    }
-    return tuple[PROMISE];
-  }, depArray);
+    return fnAsync
+      ? fnAsync()
+          .then<ReturnTupleType<T>>(data => [data, false, undefined, tuple[REFRESH], tuple[PROMISE]])
+          .catch<ReturnTupleType<T>>((err: unknown) => [undefined, false, err, tuple[REFRESH], tuple[PROMISE]])
+          .then(tuple => {
+            setTuple(tuple);
+            console.log("RETURNING");
+            return tuple;
+          })
+      : tuple[PROMISE];
+  }, dependencies);
 
-  // calls above refresh fn which will also save the returned promise
-  useMemo(tuple[REFRESH], depArray);
+  // conditional refresh, conditional on dependecies automatic change & not already emitted fetch
+  useMemo(() => {
+    console.log("CALLING?");
+    if (
+      !Array.isArray(dependencies) || // if dependencies is missing or not an array, call everytime
+      !args.current || // if we've never done this before, call it
+      args.current.length != dependencies.length || // if the arrays differ in size or shallow contents, call it
+      args.current.some((arg, i) => !Object.is(arg, dependencies[i]))
+    ) {
+      console.log("CALLING...");
+      args.current = dependencies;
+      tuple[REFRESH]();
+    }
+    //return tuple[PROMISE];
+  }, dependencies);
 
   return tuple;
 }
